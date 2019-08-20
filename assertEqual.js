6cdf6179b45756c2e7d2fc5c1eca1fb1585ebd32
kirillradaev <cyrilrdv@gmail.com>
2019-08-20 00:32:51 +0000

    Changes

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
    if (actual === expected) {
-     console.log('🙌Assertion Passed: [actual] === [expected]');
+     console.log('😀Assertion Passed: [actual] === [expected]');
    } else {
-   console.log('😥Assertion Failed: [actual] !==  [expected]');
+    console.log('😞Assertion Failed: [actual] !== [expected]');
    }
 };
 
