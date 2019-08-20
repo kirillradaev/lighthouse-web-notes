a2b535a85357bca08742a357c95b2240d33528e7
kirillradaev <cyrilrdv@gmail.com>
2019-08-20 00:24:26 +0000

    Assert assinment

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+   if (actual === expected) {
+     console.log('🙌Assertion Passed: [actual] === [expected]');
+   } else {
+   console.log('😥Assertion Failed: [actual] !==  [expected]');
+   }
+};
+
+assertEqual('Lighhouse Labs', 'Bootcamp');
+assertEqual(1, 1);
