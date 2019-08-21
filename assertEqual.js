6c3b3befcd381ee8f25de1afe4e5acfd2d790d4b
kirillradaev <cyrilrdv@gmail.com>
2019-08-21 19:50:55 +0000

    Created a new function

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
-   if (actual === expected) {
-     console.log(`😀Assertion Passed:${actual === expected}`);
-   } else {
-    console.log(`😞Assertion Failed:${actual !== expected}`);
-   }
+  if (actual === expected) {
+    console.log(`😀Assertion Passed:${actual} === ${expected}`);
+  } else {
+   console.log(`😞Assertion Failed:${actual} !== ${expected}`);
+  }
 };
-
 assertEqual('Lighhouse Labs', 'Bootcamp');
 assertEqual(1, 1);
