a2155bad5db502f6b236937711de855ec81117f4
kirillradaev <cyrilrdv@gmail.com>
2019-08-21 22:53:34 +0000

    Created a without function

---
+++ without.js
@@ -0,0 +1,47 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+    } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+      return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1,arr2){
+  if(eqArrays(arr1,arr2)){
+    console.log(`😀Assertion Passed:${arr1} === ${arr2}`);
+  }else{
+    console.log(`😞Assertion Failed:${arr1} !== ${arr2}`);
+  }
+};
+
+
+const source = [];
+const itemsToRemove = [];
+
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+   for (let i = 0 ;i < source.length; i++){
+     let found = false; 
+     for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]){
+        found=true;
+        }
+      } 
+      if(!found) {
+        newArr.push(source[i]); 
+      }
+    }
+    return newArr;
+};
+
+
+
+
+
+assertArraysEqual(without([1, 2, 3], [1]), [2,3]); // => [2, 3]
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ['1', '2']); // => ["1", "2"]
